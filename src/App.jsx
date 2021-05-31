cea8c3bc1cf7c2122d54960ad5cc67bc2d7b5446
koki-noguchi <67818050+koki-noguchi@users.noreply.github.com>
2021-05-31 19:52:31 +0900

    jsxのCSSの書き方を試した {}でJavascriptを埋め込み、{{}}でjavascriptのオブジェクトを埋め込む

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
 import React from "react";
 
 const App = () => {
+  const onClickButton = () => {
+    return alert();
+  };
+  const contentStyle = {
+    color: "blue",
+    textSize: "16px"
+  };
   return (
     <>
-      <h1>こんにちは</h1>
-      <p>reactです</p>
+      <h1 style={{ color: "green" }}>こんにちは</h1>
+      <p style={contentStyle}>reactです</p>
+      <button onClick={onClickButton}>ボタン</button>
     </>
   );
 };
