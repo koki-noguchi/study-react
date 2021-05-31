577862dc1bd15840094c39edcfea7b9b9f8d782b
koki-noguchi <67818050+koki-noguchi@users.noreply.github.com>
2021-05-31 21:36:08 +0900

    useEffectを試した 再レンダリングの際に、useEffectの第二引数で指定した値が変化した時のみ、第一引数を実行できる。

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,29 @@
-import React, { useState } from "react";
+/* eslint react-hooks/exhaustive-deps: off */
+import React, { useEffect, useState } from "react";
 import ColorfulMessage from "./components/ColorfulMessage";
 
 const App = () => {
+  const [num, setNum] = useState(0);
+  const [showFaceFlag, setFaceFlag] = useState(true);
+
   const onClickCountUp = () => {
     setNum(num + 1);
   };
 
-  const [num, setNum] = useState(0);
+  const onClickSwichShowFlag = () => {
+    setFaceFlag(!showFaceFlag);
+  };
+
+  useEffect(() => {
+    if (num > 0) {
+      if (num % 3 === 0) {
+        showFaceFlag || setFaceFlag(true);
+      } else {
+        showFaceFlag && setFaceFlag(false);
+      }
+    }
+  }, [num]);
+
   return (
     <>
       <h1 style={{ color: "green" }}>こんにちは</h1>
@@ -14,6 +31,9 @@ const App = () => {
       <ColorfulMessage color="grey">react勉強中です</ColorfulMessage>
       <button onClick={onClickCountUp}>カウントアップ</button>
       <p>{num}</p>
+      <button onClick={onClickSwichShowFlag}>on/off</button>
+      <br></br>
+      {showFaceFlag && <p>^ ^</p>}
     </>
   );
 };
