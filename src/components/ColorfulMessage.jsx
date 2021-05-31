9bac8ede23d9a1f7769390dd63f9e0019b9ead62
koki-noguchi <67818050+koki-noguchi@users.noreply.github.com>
2021-05-31 21:42:29 +0900

    コンポーネントのexport defaultをexportに変更 exportであれば、分割代入で宣言した名称がファイル名と異なれば、エラーを吐いてくれる

--- src/components/ColorfulMessage.jsx
+++ src/components/ColorfulMessage.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const ColorfulMessage = (props) => {
+export const ColorfulMessage = (props) => {
   const { color, children } = props;
   const contentStudyStyle = {
     color: color,
@@ -9,5 +9,3 @@ const ColorfulMessage = (props) => {
 
   return <p style={contentStudyStyle}>{children}</p>;
 };
-
-export default ColorfulMessage;
